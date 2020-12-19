388bc9f2e912c744505bc4813e7186c2193afd92
smaraujo <sanderson.mda@gmail.com>
2020-12-19 15:50:03 -0300

    developed delete by id operation

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -40,4 +40,10 @@ public class PersonController {
     public PersonDTO findById(@PathVariable long id) throws PersonNotFoundException {
        return personService.findById(id);
     }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long  id) throws PersonNotFoundException {
+       personService.delete(id);
+    }
 }

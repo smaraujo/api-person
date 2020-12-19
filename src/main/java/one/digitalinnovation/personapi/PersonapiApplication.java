17d481a48fd14c2464df4cc3b1fd19e4b313f093
smaraujo <sanderson.mda@gmail.com>
2020-12-19 01:34:30 -0300

    desenvolvendo lista com todos metodos

--- src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
+++ src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
@@ -2,8 +2,13 @@ package one.digitalinnovation.personapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"one.digitalinnovation.personapi.Services"})
+
+
 public class PersonapiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,19 +1,23 @@
 package one.digitalinnovation.personapi.controller;
 
-
-
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
-import one.digitalinnovation.personapi.entity.Person;
+
 import one.digitalinnovation.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-   private PersonService personService;
+   private  PersonService personService;
 
    @Autowired
     public PersonController(PersonService personService) {
@@ -22,7 +26,8 @@ public class PersonController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public MessageResponseDTO createPerson(@RequestBody Person person) {
-       return  personService.createPerson(person);
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
+
+       return  personService.createPerson(personDTO);
     }
 }
